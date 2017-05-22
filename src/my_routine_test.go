d230e14abf71ff18b0b320e019273915733b2193
yowcow <yowcow@cpan.org>
2017-05-22 19:44:01 +0900

    wait group

--- src/my_routine_test.go
+++ src/my_routine_test.go
@@ -60,3 +60,69 @@ func TestSumLocked(t *testing.T) {
 	assert.Equal(15, res1)
 	assert.Equal(20, res2)
 }
+
+func fib(c, q chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-q:
+			return
+		}
+	}
+}
+
+func TestReceiverToQuitSender(t *testing.T) {
+	assert := assert.New(t)
+
+	c := make(chan int)
+	q := make(chan int)
+	ret := []int{}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			ret = append(ret, <-c)
+		}
+		q <- 0
+		wg.Done()
+	}()
+
+	fib(c, q)
+	wg.Wait()
+
+	assert.EqualValues([]int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}, ret)
+}
+
+func TestBoomAfterTick(t *testing.T) {
+	assert := assert.New(t)
+
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(300 * time.Millisecond)
+	ret := []string{}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		for {
+			select {
+			case <-tick:
+				ret = append(ret, "tick")
+			case <-boom:
+				ret = append(ret, "boom")
+				wg.Done()
+			default:
+				ret = append(ret, ".")
+				time.Sleep(50 * time.Millisecond)
+			}
+		}
+	}()
+
+	wg.Wait()
+
+	assert.True(9 <= len(ret))
+}
