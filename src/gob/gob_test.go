5c6b20d857b8c36505acadc382acba13a0c2f265
yowcow <yowcow@cpan.org>
2018-06-24 20:57:47 +0900

    decode to different type

--- src/gob/gob_test.go
+++ src/gob/gob_test.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type Request struct {
@@ -16,6 +17,7 @@ type Request struct {
 }
 
 type Response struct {
+	Time     *time.Time
 	ReplyFor *Request
 }
 
@@ -53,7 +55,8 @@ func startServer() (string, <-chan bool) {
 		if err := dec.Decode(&req); err != nil {
 			panic(err)
 		}
-		if err := enc.Encode(Response{&req}); err != nil {
+		now := time.Now()
+		if err := enc.Encode(Response{&now, &req}); err != nil {
 			panic(err)
 		}
 	}()
@@ -83,6 +86,45 @@ func TestGobMessaging(t *testing.T) {
 
 	<-done // wait for server to quit
 
+	if !reflect.DeepEqual(req, *resp.ReplyFor) {
+		t.Errorf("expected %v but got %v", req, *resp.ReplyFor)
+	}
+}
+
+type Response2 struct {
+	Number    int
+	NumberPtr *int
+	ReplyFor  *Request
+}
+
+func TestGobMessagingDecodeToDifferntType(t *testing.T) {
+	socket, done := startServer()
+	conn, err := net.Dial("unix", socket)
+	if err != nil {
+		panic(err)
+	}
+
+	enc := gob.NewEncoder(conn)
+	dec := gob.NewDecoder(conn)
+
+	req := Request{"fuga", 30}
+	if err := enc.Encode(req); err != nil {
+		t.Fatalf("expected nil but got %v", err)
+	}
+
+	var resp Response2
+	if err := dec.Decode(&resp); err != nil {
+		t.Fatalf("expected nil but got %v", err)
+	}
+
+	<-done // wait for server to quit
+
+	if resp.Number != 0 {
+		t.Errorf("expected 0 but got %v", resp.Number)
+	}
+	if resp.NumberPtr != nil {
+		t.Errorf("expected nil but got %v", resp.NumberPtr)
+	}
 	if !reflect.DeepEqual(req, *resp.ReplyFor) {
 		t.Errorf("expected %v but got %v", req, *resp.ReplyFor)
 	}
