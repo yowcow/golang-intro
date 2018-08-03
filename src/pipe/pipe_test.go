bd261881c37f8449458bb4534dbbe8d223dfd6fb
yowcow <yowcow@cpan.org>
2018-08-03 18:57:02 +0900

    process pipelining

--- src/pipe/pipe_test.go
+++ src/pipe/pipe_test.go
@@ -82,16 +82,9 @@ func TestSubprocessStdinPipe(t *testing.T) {
 	if _, err := io.WriteString(w, "fugafuga\n"); err != nil {
 		t.Error("expected nil but got", err)
 	}
+
+	// close input to subprocess and wait for it to finish
 	w.Close()
-
-	// Await a little to make sure buffer is flushed to file.
-	// This shouldn't be a good practice.
-	time.Sleep(100 * time.Millisecond)
-
-	if err := cmd.Process.Kill(); err != nil {
-		t.Fatal("expected nil but got", err)
-	}
-
 	cmd.Wait()
 
 	f, err := os.Open(expectedFile)
@@ -108,5 +101,54 @@ fugafuga
 `
 	if string(b) != expectedOutput {
 		t.Error("expected", expectedOutput, "but got", string(b))
+	}
+}
+
+func TestStdoutStdinPipelining(t *testing.T) {
+	cmd1 := exec.Command("cat", "data.txt")
+	cmd1Stdout, err := cmd1.StdoutPipe()
+	if err != nil {
+		t.Fatal("expected nil but got", err)
+	}
+
+	cmd2 := exec.Command("grep", "hoge")
+	cmd2.Stdin = cmd1Stdout
+	cmd2Stdout, err := cmd2.StdoutPipe()
+	if err != nil {
+		t.Fatal("expected nil but got", err)
+	}
+
+	cmd3 := exec.Command("grep", "-v", "fuga")
+	cmd3.Stdin = cmd2Stdout
+	r, err := cmd3.StdoutPipe()
+	if err != nil {
+		t.Fatal("expected nil but got", err)
+	}
+
+	if err := cmd1.Start(); err != nil {
+		t.Fatal("expected nil but got", err)
+	}
+
+	if err := cmd2.Start(); err != nil {
+		t.Fatal("expected nil but got", err)
+	}
+
+	if err := cmd3.Start(); err != nil {
+		t.Fatal("expected nil but got", err)
+	}
+
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		t.Fatal("expected nil but got", err)
+	}
+
+	// wait for all processes to finish
+	cmd1.Wait()
+	cmd2.Wait()
+	cmd3.Wait()
+
+	expected := "hogehoge\n"
+	if string(data) != expected {
+		t.Error("expected", expected, "but got", string(data))
 	}
 }
