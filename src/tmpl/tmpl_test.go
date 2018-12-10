62d337d8b9e51ee2257bce05dcdabe079841e0df
yowcow <yowcow@cpan.org>
2018-12-10 13:53:04 +0900

    template

---
+++ src/tmpl/tmpl_test.go
@@ -0,0 +1,25 @@
+package tmpl
+
+import (
+	"bytes"
+	"html/template"
+	"testing"
+)
+
+func TestHelloWorld(t *testing.T) {
+	tpl, err := template.New("foo").Parse(`{{define "T"}}Hello, {{.}}!{{end}}`)
+	if err != nil {
+		t.Fatal("expected no error but got", err)
+	}
+
+	var buf bytes.Buffer
+	err = tpl.ExecuteTemplate(&buf, "T", "<script>pwned!</script>")
+	if err != nil {
+		t.Fatal("expected no error but got", err)
+	}
+
+	expected := `Hello, &lt;script&gt;pwned!&lt;/script&gt;!`
+	if buf.String() != expected {
+		t.Error("expected", expected, "but got", buf.String())
+	}
+}
